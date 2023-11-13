cace9a5afc47c29ee9298087d6f2372085cd8831
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-11-13 17:01:35 +0100

    fix: ignore path for sync state

--- components/element.vue
+++ components/element.vue
@@ -13,10 +13,18 @@ const conceptParamKey = props.element.concept && `_c_${props.element.concept['x-
     :src="`/data-fair/embed/dataset/${element.dataset.id}/table?display=${element.display}`"
     :query-params-include="[conceptParamKey]"
     :query-params-exclude="paramFields.filter(f => f !== conceptParamKey)"
+    :sync-state-ignore-path="true"
     :sync-state="true"
     :style="`height:${height}px`"
   />
-  <v-iframe v-else-if="element.type === 'application'" :src="`/data-fair/app/${element.application.id}`" :query-params-include="[conceptParamKey]" :query-params-exclude="paramFields.filter(f => f !== conceptParamKey)" :sync-state="true" />
+  <v-iframe
+    v-else-if="element.type === 'application'"
+    :src="`/data-fair/app/${element.application.id}`"
+    :query-params-include="[conceptParamKey]"
+    :query-params-exclude="paramFields.filter(f => f !== conceptParamKey)"
+    :sync-state="true"
+    :sync-state-ignore-path="true"
+  />
   <div v-else-if="element.type === 'text'">
     {{ element.content }}
   </div>
