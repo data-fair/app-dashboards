54e64d76b4b6fbf0edd865e5de5b6c05b35c439d
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-11-13 12:01:52 +0100

    feat: can set initial value for filter

--- app.vue
+++ app.vue
@@ -12,6 +12,13 @@ if (incompleteConfiguration) {
 }
 const dataset = await $fetch(config.datasets[0].href)
 const labelField = dataset.schema.find(f => f.key === config.labelField || f['x-refersTo'] === 'http://www.w3.org/2000/01/rdf-schema#label')
+const route = useRoute()
+const router = useRouter()
+if (!route.query[labelField.key] && config.startValue) {
+  const newQuery = { ...route.query }
+  newQuery[labelField.key] = config.startValue
+  router.replace({ path: route.path, query: newQuery })
+}
 
 const conceptsFields = [].concat(...config.sections.map(s => [].concat(...s.elements.filter(e => e.concept).map(e => e.concept)))).filter((e1, i, s) => s.findIndex(e2 => e1.key === e2.key) === i)
 const paramFields = [labelField.key].concat(conceptsFields.map(f => `_c_${f['x-concept'].id}_eq`))
@@ -28,7 +35,7 @@ const conceptValues = ref(null)
     </p>
     <v-row class="mt-3">
       <v-spacer />
-      <v-col cols="auto">
+      <v-col v-if="conceptsFields" cols="auto">
         <concept-filter :dataset="dataset" :label-field="labelField" :concepts-fields="conceptsFields" @update:model-value="value => conceptValues = value" />
       </v-col>
       <v-spacer />
