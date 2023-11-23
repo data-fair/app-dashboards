c37f83da022438066bf0d82bdf2ad033f79ee2cc
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-11-23 12:21:50 +0100

    fix: do not send concept Param if value is undefined

--- src/components/dashboard-element.vue
+++ src/components/dashboard-element.vue
@@ -13,7 +13,7 @@ const conceptParamKey = props.element.concept && `_c_${props.element.concept['x-
   <v-iframe
     v-if="element.type === 'tablePreview'"
     :src="`/data-fair/embed/dataset/${element.dataset.id}/table?display=${element.display}&interaction=${!element.noInteractions}${element.fields.length ? ('&cols=' + element.fields.join(',')) : ''}`"
-    :query-params-extra="{[conceptParamKey]: reactiveSearchParams[conceptParamKey] }"
+    :query-params-extra="reactiveSearchParams[conceptParamKey] ? {[conceptParamKey]: reactiveSearchParams[conceptParamKey] } : {}"
     :style="`height:${height>0 ? height+'px' : '100%'}`"
   />
   <div
@@ -23,7 +23,7 @@ const conceptParamKey = props.element.concept && `_c_${props.element.concept['x-
     <v-iframe
       v-if="element.type === 'application'"
       :src="`/data-fair/app/${element.application.id}`"
-      :query-params-extra="{ [conceptParamKey]: reactiveSearchParams[conceptParamKey] }"
+      :query-params-extra="reactiveSearchParams[conceptParamKey] ? {[conceptParamKey]: reactiveSearchParams[conceptParamKey] } : {}"
       :style="element.application.baseApp.meta['df:overflow'] !== 'true' ? `height:${height>0 ? height+'px' : '100%'}` : ''"
     />
     <div v-else-if="element.type === 'text'">
